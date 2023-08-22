f101df307ee1f199102f9a7daf8192335eead615
Ankita Kumari <ankita00iv@gmail.com>
2023-08-22 20:39:22 +0530

    Create arrayptr.cpp

---
+++ arrayptr.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+void g(int *ptr);
+int main()
+{
+  int age = 20;
+  g(&age);
+  cout << age << endl;
+  return 0;
+}
+void g(int *ptr)
+{
+  cout << *ptr << endl;
+  *ptr = 100;
+}
