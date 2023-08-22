a10b076bd436d2aa4ed1209a4049f82c7bc7aea3
Ankita Kumari <ankita00iv@gmail.com>
2023-08-22 23:56:34 +0530

    address  in pointer

---
+++ pointeraddress.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+struct student
+{
+    int rollno;
+    char sex;
+    int age;
+};
+void display(student a);
+void show(student *a);
+
+int main()
+{
+    student anki = {12, 'f', 18};
+    display(anki);
+    cout << endl;
+    show(&anki);
+    display(anki);
+    return 0;
+}
+void display(student a)
+{
+    cout << a.rollno << endl;
+    cout << a.sex << endl;
+    cout << a.age << endl;
+    a.rollno = 00;
+}
+void show(student *a)
+{
+    cout << a->rollno << endl;
+    cout << a->sex << endl;
+    cout << a->age << endl;
+    a->rollno = 000;
+}
