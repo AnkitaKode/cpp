4b6f67337cf38cbf048d937e021aeafac55033a9
Ankita Kumari <ankita00iv@gmail.com>
2023-09-08 00:14:12 +0530

    initial commit

---
+++ Deque.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <deque>
+
+using namespace std;
+int main()
+{
+    deque<int> d;
+    d.push_back(1);
+    d.push_back(2);
+
+    cout << endl;
+    cout << "Print First Index Element->" << d.at(1) << endl;
+    cout << "front " << d.front() << endl;
+    cout << "back " << d.back() << endl;
+
+    cout << "Empty or not" << d.empty() << endl;
+
+    cout << "before erase" << d.size() << endl;
+    d.erase(d.begin(), d.begin() + 1);
+    cout << "after erase" << d.size()
+         << endl;
+    for (int i : d)
+    {
+        cout << i << endl;
+    }
+    return 0;
+}
