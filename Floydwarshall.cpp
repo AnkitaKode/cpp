0de371cd50ff6bfc39b3486adf50b86ef45c231e
Ankita Kumari <ankita00iv@gmail.com>
2025-01-11 23:57:06 +0530

    initial commit

--- Floydwarshall.cpp
+++ Floydwarshall.cpp
@@ -37,7 +37,15 @@ void printSolution(int dist[][V])
             " between every pair of vertices \n";
     for (int i = 0; i < V; i++)
     {
-       
+        for (int j = 0; j < V; j++)
+        {
+            if (dist[i][j] == INF)
+                cout << "INF"
+                     << " ";
+            else
+                cout << dist[i][j] << "   ";
+        }
+        cout << endl;
     }
 }
 
