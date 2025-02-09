23da295165f35bff560e35dc5e87469a21fe0159
Ankita Kumari <ankita00iv@gmail.com>
2025-02-09 23:20:13 +0530

    initial commit

---
+++ DynamicBinding.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class Content
+{
+public:
+    void call_Function()
+    {
+        print();
+    }
+    void print()
+    {
+        cout << "Printing the Base class Content" << endl;
+    }
+};
+
+class Content2 : public Content
+{
+public:
+    void print()
+    {
+        cout << "Printing the Derived class Content" << endl;
+    }
+};
+
+int main()
+{
+    Content *geeksforgeeks = new Content();
+    geeksforgeeks->call_Function();
+
+    Content *geeksforgeeks2 = new Content2();
+    geeksforgeeks2->call_Function();
+    delete geeksforgeeks;
+    delete geeksforgeeks2;
+
+    return 0;
+}
